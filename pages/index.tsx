cab830d6049c0e777718da5cb274a9221af31865
Marina Bovkunovich <marina.b@abiatec.com>
2022-08-23 12:45:09 +0300

    feat: add loading image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 export default function Gallery() {
 	return (
 		<div className='max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8'>
@@ -12,7 +14,7 @@ function BlurImage() {
 	return (
 		<a href="#" className='group'>
 			<div className='aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full overflow-hidden rounded-lg bg-gray-200'>
-				<img src="https://bit.ly/placeholder-img" alt="" className='group-hover:opacity-75' />
+				<Image src="https://bit.ly/placeholder-img" alt="" layout='fill' objectFit='cover' className='group-hover:opacity-75' />
 			</div>
 			<h3 className='mt-4 text-sm text-gray-700'>Marina</h3>
 			<p className='mt-1 text-lg font-medium text-gray-900'>text</p>
